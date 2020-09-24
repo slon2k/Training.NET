6dc5e927ba7d9960744a368877a57f32c772a10f
Boris Korobeinikov <slon2k@gmail.com>
2020-09-24 22:55:08 +0600

    init

---
+++ Module01/App/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="Boris Korobeinikov">
+//  Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace App
+{
+    using System;
+
+    /// <summary>
+    /// Starting point for application.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module01/BasicCoding/Task01.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Task01.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding
+{
+    using System;
+
+    /// <summary>
+    /// Task 1.
+    /// Implement the InsertNumber algorithm for inserting bits from the j-th to the i-th of one number into another.
+    /// </summary>
+    public static class Task01
+    {
+        /// <summary>
+        /// Inserts j-th to i-th bits from one integer to another.
+        /// </summary>
+        /// <param name="a">The first integer.</param>
+        /// <param name="b">The second integer.</param>
+        /// <param name="i">The first bit.</param>
+        /// <param name="j">The last bit.</param>
+        /// <returns>The modified number.</returns>
+        public static int InsertNumber(int a, int b, int i, int j) => throw new NotImplementedException();
+    }
+}
