4d560bc2899f77d7b1905aec71348ffefe11ddec
Boris Korobeinikov <slon2k@gmail.com>
2020-09-25 22:49:16 +0600

    Created: Task02

---
+++ Module01/BasicCoding.Tests/Task02Tests.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Task02Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for Task 2.
+    /// </summary>
+    [TestFixture]
+    internal class Task02Tests
+    {
+        /// <summary>
+        /// Finding maximum element in the array.
+        /// </summary>
+        /// <param name="a">Array of integers.</param>
+        /// <returns>Maximum element.</returns>
+        [TestCase(new int[] { 1, 2, 4, 6, 4, 0 }, ExpectedResult = 6)]
+        [TestCase(new int[] { 3, 2, 1 }, ExpectedResult = 3)]
+        [TestCase(new int[] { -3, -2, -1 }, ExpectedResult = -1)]
+        internal int CheckFindMax(int[] a)
+        {
+            return Task02.FindMax(a);
+        }
+    }
+}
---
+++ Module01/BasicCoding/Task02.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Task02.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding
+{
+    /// <summary>
+    /// Task 2.
+    /// Implement a recursive algorithm for finding the maximum element in an unsorted integer array.
+    /// </summary>
+    public static class Task02
+    {
+        /// <summary>
+        /// Finds maximum element in array.
+        /// </summary>
+        /// <param name="a">Array of integers.</param>
+        /// <returns>Maximum element.</returns>
+        public static int FindMax(int[] a)
+        {
+            return 0;
+        }
+    }
+}
