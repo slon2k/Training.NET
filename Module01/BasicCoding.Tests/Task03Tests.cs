dc2f510809ee96667cd6e58e9572ae3c52583593
Boris Korobeinikov <slon2k@gmail.com>
2020-09-26 16:18:16 +0600

    added: Task 03

---
+++ Module01/BasicCoding.Tests/Task03Tests.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="Task03Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for Task 3.
+    /// </summary>
+    [TestFixture]
+    public class Task03Tests
+    {
+        /// <summary>
+        /// Finds an element for which the sum of the elements on the left
+        /// and the sum of the elements on the right are equal.
+        /// </summary>
+        /// <param name="a">Array of double.</param>
+        /// <returns>Index of element or -1.</returns>
+        [TestCase(new double[] { -3.0, -2.5, -1.5 }, ExpectedResult = -1)]
+        [TestCase(new double[] { 1.0, 1.0, 5.5, 2.0 }, ExpectedResult = 2)]
+        [TestCase(new double[] { 1.0, 1.0, 2.0, 1.0, 2.0, 5.0 }, ExpectedResult = 4)]
+        [TestCase(new double[] { 1.0 }, ExpectedResult = 0)]
+        [TestCase(new double[] { 1.0, 2.0 }, ExpectedResult = -1)]
+        public int CheckFindIndex(double[] a)
+        {
+            return Task03.FindIndex(a);
+        }
+    }
+}
---
+++ Module01/BasicCoding/Task03.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Task03.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding
+{
+    /// <summary>
+    /// Task 3.
+    /// Implement the search algorithm in a real array for an element index for which the sum of the elements on the left
+    /// and the sum of the elements on the right are equal.
+    /// </summary>
+    public static class Task03
+    {
+        /// <summary>
+        /// Finds an element for which the sum of the elements on the left
+        /// and the sum of the elements on the right are equal.
+        /// </summary>
+        /// <param name="a">Array of double.</param>
+        /// <returns>Index of element or -1.</returns>
+        public static int FindIndex(double[] a)
+        {
+            double sumTotal = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sumTotal += a[i];
+            }
+
+            double sumLeft = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sumLeft += a[i];
+                double sumRight = sumTotal - sumLeft + a[i];
+                if (sumLeft == sumRight)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
