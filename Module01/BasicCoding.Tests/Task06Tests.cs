7eff74caf8fc8a8cd136348f36aecc7a2a88aff7
Boris Korobeinikov <slon2k@gmail.com>
2020-09-26 19:13:49 +0600

    created: Task06

---
+++ Module01/BasicCoding.Tests/Task06Tests.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Task06Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for Task 6.
+    /// </summary>
+    [TestFixture]
+    public class Task06Tests
+    {
+        /// <summary>
+        /// Filters numbers containing given digit.
+        /// </summary>
+        /// <param name="sourceArray">Array.</param>
+        /// <param name="digit">Digit.</param>
+        /// <returns>Filtered array.</returns>
+        [TestCase(new int[] { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }, 7, ExpectedResult = new int[] { 7, 7, 70, 17 })]
+        [TestCase(new int[] { 7, 1, 2, 3, 4, 5, 6, 7, 68, -69, 70, 15, 17 }, 6, ExpectedResult = new int[] { 6, 68, -69 })]
+        [TestCase(new int[] { 7, 1, 2, 3, 4, 5, 6, 7, 68, -88, 70, 15, 17 }, 9, ExpectedResult = new int[] { })]
+        public int[] CheckDigitFiltering(int[] sourceArray, int digit)
+        {
+            return Task06.FilterDigit(sourceArray, digit);
+        }
+
+        /// <summary>
+        /// Checking range of given digit.
+        /// </summary>
+        /// <param name="sourceArray">Array.</param>
+        /// <param name="digit">Digit.</param>
+        [TestCase(new int[] { 7, 1 }, -1)]
+        [TestCase(new int[] { 0, 1 }, 11)]
+        public void CheckDigitFilteringArgumentOutOfRangeException(int[] sourceArray, int digit)
+        {
+            Assert.That(() => Task06.FilterDigit(sourceArray, digit), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
---
+++ Module01/BasicCoding/Task06.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Task06.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Task 6.
+    /// Implement a method that takes an array of integers and filters it so that only numbers containing the given digit remain.
+    /// </summary>
+    public static class Task06
+    {
+        /// <summary>
+        /// Filters numbers containing given digit.
+        /// </summary>
+        /// <param name="a">Array.</param>
+        /// <param name="digit">Digit.</param>
+        /// <returns>Filtered array.</returns>
+        public static int[] FilterDigit(int[] a, int digit)
+        {
+            if (!IsDigit(digit))
+            {
+                throw new ArgumentOutOfRangeException("digit is not in range");
+            }
+
+            return a;
+        }
+
+        private static bool IsDigit(int digit) => digit >= 0 && digit <= 9;
+    }
+}
