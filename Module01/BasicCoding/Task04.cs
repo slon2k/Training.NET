9b3a3c66ee07c1b93f574351129d918c9060989d
Boris Korobeinikov <slon2k@gmail.com>
2020-09-26 18:46:26 +0600

    Task04: passing tests

--- Module01/BasicCoding/Task04.cs
+++ Module01/BasicCoding/Task04.cs
@@ -15,14 +15,31 @@ namespace BasicCoding
     public static class Task04
     {
         /// <summary>
-        /// Concatenates two strings excluding duplicate characters from the second string.
+        /// Concatenates two strings excluding from the second string characters from the first string.
         /// </summary>
         /// <param name="firstString">First string.</param>
         /// <param name="secondString">Second string.</param>
         /// <returns>Concatenated string.</returns>
         public static string ConcatenateStrings(string firstString, string secondString)
         {
-            return string.Empty;
+            var charactersUsed = new HashSet<char>();
+            foreach (var character in firstString)
+            {
+                charactersUsed.Add(character);
+            }
+
+            var str = new StringBuilder();
+            str.Append(firstString);
+
+            foreach (var character in secondString)
+            {
+                if (!charactersUsed.Contains(character))
+                {
+                    str.Append(character);
+                }
+            }
+
+            return str.ToString();
         }
     }
 }
