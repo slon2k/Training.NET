b9c913c6679bb9ec58a3cf637f01d85899540cd9
Boris Korobeinikov <slon2k@gmail.com>
2020-09-26 18:25:36 +0600

    added: Task04

---
+++ Module01/BasicCoding.Tests/Task04Tests.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Task04Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for Task 4.
+    /// </summary>
+    [TestFixture]
+    public class Task04Tests
+    {
+        /// <summary>
+        /// Concatenates two strings excluding duplicate characters from the second string.
+        /// </summary>
+        /// <param name="firstStr">First string.</param>
+        /// <param name="secondStr">Second string.</param>
+        /// <returns>Concatenated string.</returns>
+        [TestCase("AsdfeAd", "Assqaasssqs", ExpectedResult = "AsdfeAdqaaq")]
+        public string CheckStringConcatenation(string firstStr, string secondStr)
+        {
+            return Task04.ConcatenateStrings(firstStr, secondStr);
+        }
+    }
+}
---
+++ Module01/BasicCoding/Task04.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Task04.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace BasicCoding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Task 4.
+    /// Implement an algorithm for concatenating two strings excluding duplicate characters.
+    /// </summary>
+    public static class Task04
+    {
+        /// <summary>
+        /// Concatenates two strings excluding duplicate characters from the second string.
+        /// </summary>
+        /// <param name="firstString">First string.</param>
+        /// <param name="secondString">Second string.</param>
+        /// <returns>Concatenated string.</returns>
+        public static string ConcatenateStrings(string firstString, string secondString)
+        {
+            return string.Empty;
+        }
+    }
+}
