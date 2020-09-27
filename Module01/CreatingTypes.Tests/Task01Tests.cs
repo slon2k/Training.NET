e638627bb04c92b39daa3b7a1fbe882a49e4163e
Boris Korobeinikov <slon2k@gmail.com>
2020-09-27 22:32:41 +0600

    added: CreatingTypes-Task01

---
+++ Module01/CreatingTypes.Tests/Task01Tests.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="Task01Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace CreatingTypes.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for Task 1.
+    /// </summary>
+    [TestFixture]
+    public class Task01Tests
+    {
+        /// <summary>
+        /// Checks finding the nth root of a real number.
+        /// </summary>
+        /// <param name="number">Number.</param>
+        /// <param name="degree">Root degree.</param>
+        /// <param name="precision">Accuracy.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.001, 0.3)]
+        [TestCase(-0.008, 3, 0.001, -0.2)]
+        [TestCase(-8, 3, 0.001, -2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void CheckFindingNthRoot(double number, int degree, double precision, double expected)
+        {
+            double result = Task01.FindNthRoot(number, degree, precision);
+
+            Assert.That(Math.Abs(expected - result) < precision);
+        }
+
+        /// <summary>
+        /// Checking arguments.
+        /// </summary>
+        /// <param name="number">Must be positive in the case of an even degree.</param>
+        /// <param name="degree">Must be natural number.</param>
+        /// <param name="precision">Must be positive.</param>
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.01, -2, 0.0001)]
+        [TestCase(0.01, 2, -1)]
+        [TestCase(0.01, 2, 0)]
+        public void CheckFindNthRootArgumentOutOfRange(double number, int degree, double precision)
+        {
+            Assert.That(() => Task01.FindNthRoot(number, degree, precision), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
---
+++ Module01/CreatingTypes/Task01.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="Task01.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace CreatingTypes
+{
+    using System;
+
+    /// <summary>
+    /// Task 1.
+    /// Implement an algorithm that allows calculating the nth root of a real number using Newton's method with a given accuracy.
+    /// </summary>
+    public static class Task01
+    {
+        /// <summary>
+        /// Finds the nth root of a real number.
+        /// </summary>
+        /// <param name="number">Number.</param>
+        /// <param name="degree">Root degree.</param>
+        /// <param name="precision">Accuracy.</param>
+        /// <returns>Root of the number.</returns>
+        public static double FindNthRoot(double number, int degree, double precision)
+        {
+            if (degree <= 0 || precision <= 0 || (degree % 2 == 0 && number < 0))
+            {
+                throw new ArgumentOutOfRangeException("Invalid arguments");
+            }
+
+            double x = number;
+            double prev = number + precision + 1;
+
+            while (Math.Abs(prev - x) >= precision)
+            {
+                prev = x;
+                x = (((degree - 1) * x) + (number / x.Power(degree - 1))) / degree;
+            }
+
+            return x;
+        }
+
+        private static double Power(this double number, int degree)
+        {
+            double result = number;
+
+            if (degree < 1)
+            {
+                throw new ArgumentOutOfRangeException("degree must be natural.");
+            }
+
+            for (int i = 1; i < degree; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
