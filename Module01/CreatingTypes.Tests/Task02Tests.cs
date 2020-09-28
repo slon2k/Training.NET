c14ee6ceaa74329451ec0471f6328bcbd151ed7c
Boris Korobeinikov <slon2k@gmail.com>
2020-09-28 14:22:59 +0600

    added: Creating Types. Task02

---
+++ Module01/CreatingTypes.Tests/Task02Tests.cs
@@ -0,0 +1,157 @@
+﻿// <copyright file="Task02Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace CreatingTypes.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using static CreatingTypes.Task02;
+
+    /// <summary>
+    /// Tests for Task 2.
+    /// </summary>
+    [TestFixture]
+    public class Task02Tests
+    {
+        private static readonly int[,] Arr1 = new int[,]
+        {
+            { 10, 11, 12 },
+            { 9, 10, 13 },
+            { 8, 11, 20 },
+        };
+
+        private static readonly int[,] ArrayUnordered = new int[,]
+        {
+            { 3, 3, 3 },
+            { 2, 2, 2 },
+            { 1, 1, 1 },
+            { 4, 4, 4 },
+            { 5, 5, 5 },
+        };
+
+        private static readonly int[,] ArrayAscending = new int[,]
+        {
+            { 1, 1, 1 },
+            { 2, 2, 2 },
+            { 3, 3, 3 },
+            { 4, 4, 4 },
+            { 5, 5, 5 },
+        };
+
+        private static readonly int[,] ArrayDescending = new int[,]
+        {
+            { 5, 5, 5 },
+            { 4, 4, 4 },
+            { 3, 3, 3 },
+            { 2, 2, 2 },
+            { 1, 1, 1 },
+        };
+
+        private static readonly object[] ComparingByMaxTestData =
+        {
+            new object[] { Arr1, 0, 1, false },
+            new object[] { Arr1, 2, 1, true },
+            new object[] { Arr1, 0, 2, false },
+        };
+
+        private static readonly object[] ComparingByMinTestData =
+        {
+            new object[] { Arr1, 0, 1, true },
+            new object[] { Arr1, 2, 1, false },
+            new object[] { Arr1, 1, 2, true },
+        };
+
+        private static readonly object[] ComparingBySumTestData =
+        {
+            new object[] { Arr1, 0, 1, true },
+            new object[] { Arr1, 2, 1, true },
+            new object[] { Arr1, 0, 2, false },
+        };
+
+        private static readonly object[] SortingTestData =
+        {
+            new object[] { ArrayUnordered, true, ArrayAscending },
+            new object[] { ArrayUnordered, false, ArrayDescending },
+        };
+
+        /// <summary>
+        /// Checking comparing by max element in a row.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCaseSource("ComparingByMaxTestData")]
+        public void CheckComparingByMaxElement(int[,] array, int row1, int row2, bool expected)
+        {
+            Assert.That(CompareByMaxElement(array, row1, row2) == expected);
+        }
+
+        /// <summary>
+        /// Checking comparing by min element in a row.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCaseSource("ComparingByMinTestData")]
+        public void CheckComparingByMinElement(int[,] array, int row1, int row2, bool expected)
+        {
+            Assert.That(CompareByMinElement(array, row1, row2) == expected);
+        }
+
+        /// <summary>
+        /// Checking comparing by sum of elements in a row.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCaseSource("ComparingBySumTestData")]
+        public void CheckComparingBySumOfRow(int[,] array, int row1, int row2, bool expected)
+        {
+            Assert.That(CompareBySumOfRow(array, row1, row2) == expected);
+        }
+
+        /// <summary>
+        /// Check sorting.
+        /// </summary>
+        /// <param name="array">Source array.</param>
+        /// <param name="ascending">Sorting order.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCaseSource("SortingTestData")]
+        public void CheckSorting(int[,] array, bool ascending, int[,] expected)
+        {
+            SortRows(array, CompareByMaxElement, ascending);
+            Assert.That(this.ArraysAreEqual(array, expected));
+            SortRows(array, CompareByMaxElement, !ascending);
+            SortRows(array, CompareByMinElement, ascending);
+            Assert.That(this.ArraysAreEqual(array, expected));
+            SortRows(array, CompareByMinElement, !ascending);
+            SortRows(array, CompareBySumOfRow, ascending);
+            Assert.That(this.ArraysAreEqual(array, expected));
+        }
+
+        private bool ArraysAreEqual(int[,] a1, int[,] a2)
+        {
+            if (a1.GetLength(0) != a2.GetLength(0) || a1.GetLength(0) != a2.GetLength(0))
+            {
+                throw new ArgumentException("Dimensions of the arrays are not the same");
+            }
+
+            for (int i = 0; i < a1.GetLength(0); i++)
+            {
+                for (int j = 0; j < a2.GetLength(1); j++)
+                {
+                    if (a1[i, j] != a2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Module01/CreatingTypes/Task02.cs
@@ -0,0 +1,142 @@
+﻿// <copyright file="Task02.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace CreatingTypes
+{
+    using System;
+
+    /// <summary>
+    /// Task 2.
+    /// Implement a "bubble" sorting method for an integer array in such a way that it would be possible to order the rows of the matrix.
+    /// </summary>
+    public static class Task02
+    {
+        /// <summary>
+        /// Sorts rows of two-dimentional array of integers.
+        /// </summary>
+        /// <param name="a">Sourse array.</param>
+        /// <param name="compare">Comparer for rows. Bool function accepting array and indexes of rows to compare.</param>
+        /// <param name="ascending">Sorting order. Ascending by default.</param>
+        public static void SortRows(int[,] a, Func<int[,], int, int, bool> compare, bool ascending = true)
+        {
+            int length = a.GetLength(0);
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Array is empty");
+            }
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (compare(a, j, j + 1) == ascending)
+                    {
+                        a.SwapRows(j, j + 1);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compares rows of two dimentional array by minimum value.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <returns>True if maximum element of the first array is bigger.</returns>
+        public static bool CompareByMaxElement(int[,] array, int row1, int row2) => array.MaxElementInRow(row1) >= array.MaxElementInRow(row2);
+
+        /// <summary>
+        /// Compares rows of two dimentional array by minimum value.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <returns>True if minimum element of the first row is bigger.</returns>
+        public static bool CompareByMinElement(int[,] array, int row1, int row2) => array.MinElementInRow(row1) >= array.MinElementInRow(row2);
+
+        /// <summary>
+        /// Compares rows of two dimentional array by sum of the elements.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <returns>True if sum of elements of the first row is bigger.</returns>
+        public static bool CompareBySumOfRow(int[,] array, int row1, int row2) => array.SumOfRow(row1) >= array.SumOfRow(row2);
+
+        private static int MaxElementInRow(this int[,] a, int row)
+        {
+            int length = a.GetLength(1);
+
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Row is empty");
+            }
+
+            int maxValue = a[row, 0];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (a[row, i] > maxValue)
+                {
+                    maxValue = a[row, i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        private static int MinElementInRow(this int[,] a, int row)
+        {
+            int length = a.GetLength(1);
+
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Row is empty");
+            }
+
+            int minValue = a[row, 0];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (a[row, i] < minValue)
+                {
+                    minValue = a[row, i];
+                }
+            }
+
+            return minValue;
+        }
+
+        private static int SumOfRow(this int[,] a, int row)
+        {
+            int length = a.GetLength(1);
+
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Row is empty");
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                sum += a[row, i];
+            }
+
+            return sum;
+        }
+
+        private static void SwapRows(this int[,] a, int index1, int index2)
+        {
+            int length = a.GetLength(1);
+            for (int i = 0; i < length; i++)
+            {
+                int temp = a[index1, i];
+                a[index1, i] = a[index2, i];
+                a[index2, i] = temp;
+            }
+        }
+    }
+}
