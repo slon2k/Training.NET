25725344e8ff0c92768e2cb7bea64e08b0f89f0a
Boris Korobeinikov <slon2k@gmail.com>
2020-10-04 22:48:20 +0600

    added: Projects to Module02

---
+++ Module02/Tasks/Class1.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Class1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace Tasks
+{
+    public class Class1
+    {
+    }
+}
