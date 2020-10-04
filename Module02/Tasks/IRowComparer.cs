9af072a474515aff6f525ed894784c16a1737e17
Boris Korobeinikov <slon2k@gmail.com>
2020-10-04 23:36:31 +0600

    added: Task01

---
+++ Module02/Tasks/IRowComparer.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IRowComparer.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks
+{
+    /// <summary>
+    /// Row comparer.
+    /// </summary>
+    public interface IRowComparer
+    {
+        /// <summary>
+        /// Compares rows of integer array.
+        /// </summary>
+        /// <param name="array">Sourse array</param>
+        /// <param name="first">Index of the first row.</param>
+        /// <param name="second">Index of the second row.</param>
+        /// <returns>True if the second row is greater.</returns>
+        public bool CompareRows(int[,] array, int first, int second);
+    }
+}
---
+++ Module02/Tasks/Task01.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="Task01.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Refactor the sorting algorithm from Module 1 (don't use delegates and standard interfaces), allowing to sorting both in ascending and descending directions,
+    /// depending on comparison criterion of the matrix rows.
+    /// </summary>
+    public static class Task01
+    {
+        /// <summary>
+        /// Sorts rows of given array.
+        /// </summary>
+        /// <param name="array">Sourse array.</param>
+        /// <param name="comparer">Row comparer.</param>
+        public static void SortRows(int[,] array, IRowComparer comparer)
+        {
+            int length = array.GetLength(0);
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Array is empty");
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (comparer.CompareRows(array, j, j + 1) == true)
+                    {
+                        array.SwapRows(j, j + 1);
+                    }
+                }
+            }
+        }
+
+        private static void SwapRows(this int[,] a, int index1, int index2)
+        {
+            int length = a.GetLength(1);
+            for (int i = 0; i < length; i++)
+            {
+                int temp = a[index1, i];
+                a[index1, i] = a[index2, i];
+                a[index2, i] = temp;
+            }
+        }
+    }
+}
