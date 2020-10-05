909389757ca32c3f301592559adb08ce941ab257
Boris Korobeinikov <slon2k@gmail.com>
2020-10-05 12:19:04 +0600

    added: Task02

---
+++ Module02/Tasks/Task02/Circle.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="Circle.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Task02
+{
+    using System;
+
+    /// <summary>
+    /// Circle.
+    /// </summary>
+    public class Circle : Shape
+    {
+        private const double Pi = Math.PI;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Circle"/> class.
+        /// </summary>
+        /// <param name="radius">Radius.</param>
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius));
+            }
+
+            this.Radius = radius;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Circle"/> class.
+        /// </summary>
+        /// <param name="radius">Radius.</param>
+        public Circle(int radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius));
+            }
+
+            this.Radius = radius;
+        }
+
+        /// <summary>
+        /// Gets radius of the circle.
+        /// </summary>
+        public double Radius { get; }
+
+        /// <inheritdoc/>
+        public override double Perimeter() => 2 * Pi * this.Radius;
+
+        /// <inheritdoc/>
+        public override double Square() => Pi * this.Radius * this.Radius;
+    }
+}
---
+++ Module02/Tasks/Task02/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="Rectangle.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Task02
+{
+    using System;
+
+    /// <summary>
+    /// Rectangle.
+    /// </summary>
+    public class Rectangle : Shape
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle"/> class.
+        /// </summary>
+        /// <param name="side1">Side 1.</param>
+        /// <param name="side2">Side 2.</param>
+        public Rectangle(double side1, double side2)
+        {
+            if (side1 <= 0 || side2 <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Side of a rectangle must be positive.");
+            }
+
+            this.Side1 = side1;
+            this.Side2 = side2;
+        }
+
+        /// <summary>
+        /// Gets side 1.
+        /// </summary>
+        public double Side1 { get; }
+
+        /// <summary>
+        /// Gets side 2.
+        /// </summary>
+        public double Side2 { get; }
+
+        /// <inheritdoc/>
+        public override double Perimeter() => (this.Side1 + this.Side2) * 2;
+
+        /// <inheritdoc/>
+        public override double Square() => this.Side1 * this.Side2;
+    }
+}
