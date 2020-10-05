ed17d5187a22054877c0b471375f0e47ffad57f0
Boris Korobeinikov <slon2k@gmail.com>
2020-10-05 09:23:59 +0600

    tested: MinElementRowComparer

---
+++ Module02/Tasks.Tests/Task01Tests/MinElementRowComparerTests.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="MinElementRowComparerTests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Tests.Task01Tests
+{
+    using System;
+    using NUnit.Framework;
+    using Tasks.Task01;
+
+    /// <summary>
+    /// Tests for MaxElementRowComparer.
+    /// </summary>
+    [TestFixture]
+    public class MinElementRowComparerTests
+    {
+        private static readonly int[,] Arr = new int[,]
+        {
+            { 10, 11, 12 },
+            { 9, 10, 13 },
+            { 8, 11, 20 },
+        };
+
+        private static readonly object[] ComparingByMinTestData =
+        {
+            new object[] { Arr, 0, 1, true },
+            new object[] { Arr, 2, 1, false },
+            new object[] { Arr, 0, 2, true },
+        };
+
+        /// <summary>
+        /// Checking comparing by min element in a row.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="row1">First row index.</param>
+        /// <param name="row2">Second row index.</param>
+        /// <param name="expected">Expected result.</param>
+        [TestCaseSource("ComparingByMinTestData")]
+        public void CheckComparingByMinElement(int[,] array, int row1, int row2, bool expected)
+        {
+            var comparer = new MinElementRowComparer();
+            Assert.That(comparer.CompareRows(array, row1, row2), Is.EqualTo(expected));
+        }
+
+    }
+}
---
+++ Module02/Tasks/Task01/MinElementRowComparer.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="MinElementRowComparer.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Task01
+{
+    using System;
+
+    /// <summary>
+    /// Compares rows by maximum element in a row.
+    /// </summary>
+    public class MinElementRowComparer : IRowComparer
+    {
+        /// <summary>
+        /// Compares rows of integer array by minimum element.
+        /// </summary>
+        /// <param name="array">Sourse array.</param>
+        /// <param name="first">Index of the first row.</param>
+        /// <param name="second">Index of the second row.</param>
+        /// <returns>True if the second row is greater.</returns>
+        public bool CompareRows(int[,] array, int first, int second) => MinElementInRow(array, first) >= MinElementInRow(array, second);
+
+        private static int MinElementInRow(int[,] array, int row)
+        {
+            int length = array.GetLength(1);
+
+            if (length == 0)
+            {
+                throw new ArgumentOutOfRangeException("Row is empty");
+            }
+
+            int minValue = array[row, 0];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (array[row, i] < minValue)
+                {
+                    minValue = array[row, i];
+                }
+            }
+
+            return minValue;
+        }
+    }
+}
