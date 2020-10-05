5c06ee6295e6f6b45c8cc0fc32facbaabcd1ed05
Boris Korobeinikov <slon2k@gmail.com>
2020-10-05 15:19:00 +0600

    added: Polynomial

---
+++ Module02/Tasks.Tests/Task03Tests/PolynomialTests.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="PolynomialTests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Tests.Task03Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+    using Tasks.Task03;
+
+    /// <summary>
+    /// Tests for Polynomial.
+    /// </summary>
+    [TestFixture]
+    public class PolynomialTests
+    {
+        /// <summary>
+        /// Checking string expression.
+        /// </summary>
+        /// <param name="coefficients">Coefficients.</param>
+        /// <returns>Expression.</returns>
+        [TestCase(new double[] { 1, 2, 1 }, ExpectedResult = "x^2 + 2x + 1")]
+        [TestCase(new double[] { 1, -2, 1 }, ExpectedResult = "x^2 - 2x + 1")]
+        [TestCase(new double[] { 1, 0, 1 }, ExpectedResult = "x^2 + 1")]
+        [TestCase(new double[] { 1, 0, 0 }, ExpectedResult = "x^2")]
+        [TestCase(new double[] { 3, 2, 1, 1 }, ExpectedResult = "3x^3 + 2x^2 + 2x + 1")]
+        public string CheckPolynomialExpression(double[] coefficients)
+        {
+            var polynomial = new Polynomial(coefficients);
+            return polynomial.Expression();
+        }
+    }
+}
---
+++ Module02/Tasks/Task03/Polynomial.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="Polynomial.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Task03
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// A polynomial in a single indeterminate.
+    /// </summary>
+    public class Polynomial
+    {
+        private double[] coefficients;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Polynomial"/> class.
+        /// </summary>
+        /// <param name="coefficients">Coefficients.</param>
+        public Polynomial(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException(nameof(coefficients));
+            }
+
+            if (coefficients.Length == 0)
+            {
+                throw new ArgumentException("At least one coefficient is required for a polynomial");
+            }
+
+            if (coefficients.Length > 1 && coefficients[0] == 0)
+            {
+                throw new ArgumentException("The first coefficient must not be 0");
+            }
+
+            int length = coefficients.Length;
+
+            this.coefficients = new double[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                this.coefficients[i] = coefficients[length - 1 - i];
+            }
+        }
+
+        /// <summary>
+        /// Gets the the degree of the polynomial.
+        /// </summary>
+        public int Degree => this.coefficients.Length - 1;
+
+        /// <summary>
+        /// Polynomial expression like x^3 + 3x^2 + 3x + 1.
+        /// </summary>
+        /// <returns>String with expression.</returns>
+        public string Expression()
+        {
+            if (this.Degree == 0)
+            {
+                return this.coefficients[0].ToString();
+            }
+
+            var str = new StringBuilder();
+
+            for (int i = this.Degree; i >= 0; i--)
+            {
+                double coefficient = this.coefficients[i];
+
+                if (coefficient != 0)
+                {
+                    if (coefficient < 0 || i < this.Degree)
+                    {
+                        str.Append(this.Sign(coefficient));
+                    }
+
+                    if (i == 0 || coefficient != 1)
+                    {
+                        str.Append(Math.Abs(coefficient));
+                    }
+
+                    if (i > 0)
+                    {
+                        str.Append("x");
+                    }
+
+                    if (i > 1)
+                    {
+                        str.Append($"^{i}");
+                    }
+                }
+            }
+
+            return str.ToString();
+        }
+
+        private string Sign(double number) => number >= 0 ? " + " : " - ";
+    }
+}
