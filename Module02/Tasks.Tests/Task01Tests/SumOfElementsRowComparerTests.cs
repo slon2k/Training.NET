9095c886472cd7d9e9de231a8dd50f1f6e2822fc
Boris Korobeinikov <slon2k@gmail.com>
2020-10-05 09:45:00 +0600

    tested: SumOfElementsRowComparer

--- Module02/Tasks.Tests/Task01Tests/SumOfElementsRowComparerTests.cs
+++ Module02/Tasks.Tests/Task01Tests/SumOfElementsRowComparerTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="MinElementRowComparerTests.cs" company="Boris Korobeinikov">
+﻿// <copyright file="SumOfElementsRowComparerTests.cs" company="Boris Korobeinikov">
 // Copyright (c) Boris Korobeinikov. All rights reserved.
 // </copyright>
 
@@ -11,33 +11,33 @@ namespace Tasks.Tests.Task01Tests
     /// Tests for MaxElementRowComparer.
     /// </summary>
     [TestFixture]
-    public class MinElementRowComparerTests
+    public class SumOfElementsRowComparerTests
     {
         private static readonly int[,] Arr = new int[,]
-        {
-            { 10, 11, 12 },
-            { 9, 10, 13 },
-            { 8, 11, 20 },
-        };
+{
+            { 1, 1, 1 },
+            { 5, 5, 5 },
+            { 3, 3, 3 },
+};
 
-        private static readonly object[] ComparingByMinTestData =
+        private static readonly object[] ComparingBySumTestData =
         {
-            new object[] { Arr, 0, 1, true },
-            new object[] { Arr, 2, 1, false },
-            new object[] { Arr, 0, 2, true },
+            new object[] { Arr, 0, 1, false },
+            new object[] { Arr, 1, 2, true },
+            new object[] { Arr, 0, 2, false },
         };
 
         /// <summary>
-        /// Checking comparing by min element in a row.
+        /// Checking comparing by the sum of elements in a row.
         /// </summary>
         /// <param name="array">Array.</param>
         /// <param name="row1">First row index.</param>
         /// <param name="row2">Second row index.</param>
         /// <param name="expected">Expected result.</param>
-        [TestCaseSource("ComparingByMinTestData")]
-        public void CheckComparingByMinElement(int[,] array, int row1, int row2, bool expected)
+        [TestCaseSource("ComparingBySumTestData")]
+        public void CheckComparingBySumElement(int[,] array, int row1, int row2, bool expected)
         {
-            var comparer = new MinElementRowComparer();
+            var comparer = new SumOfElementsRowComparer();
             Assert.That(comparer.CompareRows(array, row1, row2), Is.EqualTo(expected));
         }
     }
