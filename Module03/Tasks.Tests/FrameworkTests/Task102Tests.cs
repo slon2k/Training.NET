5d1ab31c35dc2c312d8873572eacafefbfadea83
Boris Korobeinikov <slon2k@gmail.com>
2020-10-08 09:54:51 +0600

    added: Tests for TitleCase

---
+++ Module03/Tasks.Tests/FrameworkTests/Task102Tests.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Task102Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Tests.FrameworkTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+    using static Tasks.Framework.Task102.StringUtils;
+
+    /// <summary>
+    /// Tests for Task 2.
+    /// </summary>
+    [TestFixture]
+    public class Task102Tests
+    {
+        /// <summary>
+        /// Testing converting to title case.
+        /// </summary>
+        /// <param name="originalString">The original string to be converted.</param>
+        /// <param name="minorWords">Space-delimited list of minor words that must always be lowercase.</param>
+        /// <returns>String in title case.</returns>
+        [TestCase("a clash of KINGS", "a an the of", ExpectedResult = "A Clash of Kings")]
+        [TestCase("THE WIND IN THE WILLOWS", "The In", ExpectedResult = "The Wind in the Willows")]
+        [TestCase("the quick brown fox", ExpectedResult = "The Quick Brown Fox")]
+        public string CheckConvertingToTitleCase(string originalString, string minorWords = null)
+        {
+            return ConvertToTitleCase(originalString, minorWords);
+        }
+    }
+}
---
+++ Module03/Tasks/Framework/Task102/StringUtils.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="StringUtils.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Framework.Task102
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Write a function that will convert a string into title case, given an optional list of exceptions (minor words).
+    /// </summary>
+    public static class StringUtils
+    {
+        /// <summary>
+        /// Converts a string into title case.
+        /// </summary>
+        /// <param name="originalString">The original string to be converted.</param>
+        /// <param name="minorWords">Space-delimited list of minor words that must always be lowercase.</param>
+        /// <returns>String in title case.</returns>
+        public static string ConvertToTitleCase(string originalString, string minorWords = null)
+        {
+            return "0";
+        }
+    }
+}
