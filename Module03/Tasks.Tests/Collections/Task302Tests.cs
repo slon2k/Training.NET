83b9638bc5310fc00e91e1470474d3a16b38d460
Boris Korobeinikov <slon2k@gmail.com>
2020-10-12 21:46:07 +0600

    added: Task302

---
+++ Module03/Tasks.Tests/Collections/Task302Tests.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Task302Tests.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Tests.Collections
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using static Tasks.Collections.Task302;
+
+    /// <summary>
+    /// Tests for Task 2.
+    /// </summary>
+    public class Task302Tests
+    {
+        /// <summary>
+        /// Testing WordCount.
+        /// </summary>
+        [Test]
+        public void CheckWordCount()
+        {
+            string text = @"C# is an object-oriented, component-oriented programming language. " +
+                @"Since its origin, C# has added features to support new workloads and emerging software design practices.";
+            var frequency = WordCount(text);
+            Assert.That(frequency["c#"], Is.EqualTo(2));
+        }
+    }
+}
---
+++ Module03/Tasks/Collections/Task302.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="Task302.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace Tasks.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Implement an algorithm that allows to determine the frequency of occurrences of words in the text.
+    /// </summary>
+    public static class Task302
+    {
+        /// <summary>
+        /// Counts the frequency of occurrences of words in the text.
+        /// </summary>
+        /// <param name="text">Text.</param>
+        /// <returns>Key-value pairs. Key - word, value - number of occurrences.</returns>
+        public static IDictionary<string, int> WordCount(string text)
+        {
+            // Convert the string into an array of words.
+            string[] source = text.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var wordList = source.Select(w => w.Trim().ToLowerInvariant());
+
+            var frequency = new Dictionary<string, int>();
+
+            foreach (var word in wordList)
+            {
+                if (frequency.ContainsKey(word))
+                {
+                    frequency[word] += 1;
+                }
+                else
+                {
+                    frequency[word] = 1;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
