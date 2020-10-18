3164adcf86667dc98f0557bb3f093f6040941cbd
Boris Korobeinikov <slon2k@gmail.com>
2020-10-18 12:02:04 +0600

    added: Student, TestAssesstment

---
+++ Module03/LinqIntro/Student.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="Student.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace LinqIntro
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Text;
+
+    /// <summary>
+    /// Student.
+    /// </summary>
+    public class Student : IComparable<Student>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Student"/> class.
+        /// </summary>
+        /// <param name="firstName">First name.</param>
+        /// <param name="lastName">Last name.</param>
+        public Student(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        /// <summary>
+        /// Gets or sets first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets full name.
+        /// </summary>
+        public string FullName => $"{this.FirstName} {this.LastName}";
+
+        /// <summary>
+        /// Student comparison.
+        /// </summary>
+        /// <param name="other">Object to compare with.</param>
+        /// <returns>Integer, representing sorting order for students.</returns>
+        public int CompareTo(Student other)
+        {
+            return this.FullName.CompareTo(other.FullName);
+        }
+    }
+}
---
+++ Module03/LinqIntro/TestAssesstment.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="TestAssesstment.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace LinqIntro
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Tests results.
+    /// </summary>
+    public class TestAssesstment
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets student.
+        /// </summary>
+        public Student Student { get; set; }
+
+        /// <summary>
+        /// Gets or sets subject.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets date of a test.
+        /// </summary>
+        public DateTimeOffset TestDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets assesstment.
+        /// </summary>
+        public int Assesstment { get; set; }
+    }
+}
