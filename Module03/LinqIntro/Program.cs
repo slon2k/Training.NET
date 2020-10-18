5a903014e71e9445fa2cbd0776d09cc20ceaad5a
Boris Korobeinikov <slon2k@gmail.com>
2020-10-18 10:44:43 +0600

    added: LinqIntro Project

---
+++ Module03/LinqIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Program.cs" company="Boris Korobeinikov">
+// Copyright (c) Boris Korobeinikov. All rights reserved.
+// </copyright>
+
+namespace LinqIntro
+{
+    using System;
+
+    /// <summary>
+    /// Task 1.
+    /// Develop a console application that uses LINQ queries to retrieve and display data.
+    /// Data is stored in a binary file and represents information about the results of students’ tests.
+    /// Information about the student contains the name of the student, the name of the test,
+    /// the date of the test and the assessment of the test for this student.
+    /// To work with data the information is read into a binary search tree.
+    /// Provide the ability to define user criteria for viewing data.
+    /// Expand the ability to work with the application in such a way that users can specify the sort order and limit the number of rows retrieved.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main.
+        /// </summary>
+        /// <param name="args">Args.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
