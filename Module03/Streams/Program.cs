21dc332989bb99d26c671028ae76331149a62aac
Boris Korobeinikov <slon2k@gmail.com>
2020-10-19 12:03:00 +0600

    added: Streams Project

---
+++ Module03/Streams/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            //var source = ConfigurationManager.AppSettings["sourceFilePath"];
+
+            //var destination = ConfigurationManager.AppSettings["destinationFiePath"];
+
+            //Console.WriteLine($"ByteCopy() done. Total bytes: {ByByteCopy(source, destination)}");
+
+            //Console.WriteLine($"InMemoryByteCopy() done. Total bytes: {InMemoryByByteCopy(source, destination)}");
+
+            //Console.WriteLine(IsContentEquals(source, destination));
+        }
+    }
+}
