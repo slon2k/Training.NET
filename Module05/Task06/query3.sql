fcbe99731387c16ca29aeeb1c01b4569b28b668a
Boris Korobeinikov <slon2k@gmail.com>
2020-10-21 09:35:56 +0600

    added: Task06

---
+++ Module05/Task06/query3.sql
@@ -0,0 +1,24 @@
+SELECT 
+	CASE 
+		WHEN EmployeeID is null THEN 'All'
+		ELSE (SELECT LastName + ' ' + FirstName
+			FROM Employees AS emp
+			WHERE emp.EmployeeID = ord.EmployeeID
+        ) 
+	END AS 'Seller',
+	CASE 
+		WHEN CustomerID is null THEN 'All'
+		ELSE (SELECT CompanyName
+			FROM Customers AS cust
+			WHERE cust.CustomerID = ord.CustomerID
+		) 
+	END AS 'Customer', 
+	Amount FROM (SELECT 
+		EmployeeID, 
+		CustomerID,
+		COUNT(*) AS Amount, 
+		GROUPING(EmployeeID) AS [Grouping]
+		FROM Orders 
+		GROUP BY EmployeeID, CustomerID WITH CUBE 
+		) ord
+ORDER BY Seller, Customer, Amount DESC
